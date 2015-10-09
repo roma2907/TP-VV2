1902494a1d1edd268a46591787dd7a5cf9fdef34
romain <ramageromain@gmail.com>
2015-10-09 21:47:40 +0200

    correctif

--- Stubsmocks/simpleGame/src/main/java/test/TestSequenceDiagram.java
+++ Stubsmocks/simpleGame/src/main/java/test/TestSequenceDiagram.java
@@ -5,18 +5,30 @@ import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import simpleGame.core.Board;
 import simpleGame.core.Game;
+import simpleGame.core.Pawn;
 
-public class TestGame {
+public class TestSequenceDiagram {
 
+	private static final int SIZE_X = 6;
+	private static final int SIZE_Y = 7;
+	private static final int SIZE_X_BONUS = 3;
+	private static final int SIZE_Y_BONUS = 3;
+	private static final int NB_PAWN = 4;
+	
 	private Game game;
 	
 	private Board mockedBoard;
 	
+	private Board board;
+	
 	@Before
 	public void setUp(){
+		board = new Board(NB_PAWN,SIZE_X,SIZE_Y,SIZE_X_BONUS,SIZE_Y_BONUS);
+		board.removeAllPawns();
 		game = new Game();
 		mockedBoard = mock(Board.class);
 		game.setBoard(mockedBoard);
@@ -70,6 +82,24 @@ public class TestGame {
 		verify(mockedBoard).numberOfPawns();
 		verify(mockedBoard).maxGold();
 		assertTrue(result);
+	}
+	
+	@Test
+	public void testMaxGold(){
+		Pawn p1 = mock(Pawn.class);
+		Pawn p2 = mock(Pawn.class);
+		Mockito.when(p1.getX()).thenReturn(0);
+		Mockito.when(p1.getY()).thenReturn(0);
+		Mockito.when(p2.getX()).thenReturn(1);
+		Mockito.when(p2.getY()).thenReturn(1);
+		Mockito.when(p1.getGold()).thenReturn(1);
+		Mockito.when(p2.getGold()).thenReturn(3);
+		
+		board.addPawn(p1);
+		board.addPawn(p2);
+		int result = board.maxGold();
+		
+		assertEquals(3,result);
 	}
 
 }
